f1c558fd51c6fa073400bf64ff4ebfee2be321fa
Paul Zupan <paul.zupan.13@gmail.com>
2026-01-21 13:09:12 -0800

    Fail gracefully if no file

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,17 @@
+use colored::Colorize;
+
 fn main() {
     let mut args = std::env::args();
-    let program = args.nth(1).unwrap();
-    ecc::compile_and_link(program);
+    let program_name = args.next().unwrap(); // This should never panic
+    let Some(file_name) = args.next() else {
+        let program_name = format!("{}:", program_name).bold().white();
+        let fatal_error = "fatal error:".bold().red();
+
+        eprintln!("{} {} no input files", program_name, fatal_error);
+        eprintln!("compilation terminated.");
+
+        std::process::exit(1);
+    };
+
+    ecc::compile_and_link(file_name);
 }
