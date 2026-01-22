d3be6707bc459984c76a8a1a06777ab886e931e3
Paul Zupan <paul.zupan.13@gmail.com>
2026-01-21 16:16:00 -0800

    Add better linker messages

--- src/lib.rs
+++ src/lib.rs
@@ -14,6 +14,7 @@ pub mod lexer;
 pub mod parser;
 pub mod token;
 
+/// Run the entire compilation pipeline, taking source code to assembly.
 pub fn compile_source(source: &str) -> String {
     let tokens = lexer::tokenize(source);
     let tree = match parser::parse_token_stream(tokens) {
@@ -31,30 +32,43 @@ pub fn compile_and_link<P>(path: P)
 where
     P: AsRef<Path>,
 {
-    // Compilation
     let path = path.as_ref();
     let source = std::fs::read_to_string(path).unwrap();
     let assembly = compile_source(&source);
-
-    // Write to file
     let assembly_file = path.with_extension("s");
-    eprintln!("writing assembly to {:?}", assembly_file);
-    std::fs::write(assembly_file.clone(), assembly).unwrap();
 
-    // Link
-    let stem = path.with_extension("");
-    eprintln!("writing binary to {:?}", stem);
+    std::fs::write(assembly_file.clone(), assembly).unwrap();
+    let result = link_program(&assembly_file);
+    std::fs::remove_file(assembly_file).unwrap();
+    if !result {
+        std::process::exit(1);
+    }
+}
+
+/// Run `gcc` on the given assembly file.
+///
+/// Since I do not really feel like writing my own linker and standard library, it seems like a
+/// natural choice to link the program in this way. The return value indicates whether or not
+/// linking was successful.
+fn link_program<P>(assembly_file: P) -> bool
+where
+    P: AsRef<Path>,
+{
+    let assembly_file = assembly_file.as_ref();
+    let without_extension = assembly_file.with_extension("");
     let output = Command::new("gcc")
-        .args([assembly_file.as_os_str(), OsStr::new("-o"), stem.as_os_str()])
+        .args([
+            assembly_file.as_os_str(),
+            OsStr::new("-o"),
+            without_extension.as_os_str(),
+        ])
         .output()
         .unwrap();
 
     std::io::stdout().write_all(&output.stdout).unwrap();
     std::io::stderr().write_all(&output.stderr).unwrap();
 
-    // Remove assembly
-    eprintln!("removing {:?}", assembly_file);
-    std::fs::remove_file(assembly_file).unwrap();
+    output.status.success()
 }
 
 fn print_parse_error(e: ParseError, source: &str) {
--- src/main.rs
+++ src/main.rs
@@ -4,11 +4,11 @@ fn main() {
     let mut args = std::env::args();
     let program_name = args.next().unwrap(); // This should never panic
     let Some(file_name) = args.next() else {
-        let program_name = format!("{}:", program_name).bold().white();
-        let fatal_error = "fatal error:".bold().red();
-
-        eprintln!("{} {} no input files", program_name, fatal_error);
-        eprintln!("compilation terminated.");
+        eprintln!(
+            "{program_name}: {} {}",
+            "error:".bold().red(),
+            "no input files".bold().white()
+        );
 
         std::process::exit(1);
     };
